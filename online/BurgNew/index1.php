f8c5f4f5e2e6478659e34c086b93d630ac61f589
Tom Adriaens <tombeton@yahoo.com>
2015-05-19 23:15:45 +0200

    sticky header

--- online/BurgNew/index1.php
+++ online/BurgNew/index1.php
@@ -4,15 +4,12 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <title>Burg Vaes Arcitecten</title>
-    <
+
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.0.0/animate.min.css" rel="stylesheet" type="text/css">
-    <!-- all these references goes before the closing body tag-->
-    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="//cdn.jsdelivr.net/stickynavbar.js/1.2.0/jquery.stickyNavbar.min.js"></script>
+
     <!--[if lt IE 9]>
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
@@ -22,43 +19,49 @@
 </head>
 <body>
 <div id="wrapper">
-    <div class="first-container ">
-        <div class="v-center">
-            <h1 class="text-center header-font"><b>Burg Vaes</b></h1>
+    <div id="section1" class="scrollto">
+        <div class="first-container ">
+            <div class="v-center">
+                <h1 class="text-center header-font"><b>Burg Vaes</b></h1>
 
-            <h2 class="text-center header-font">Architecten voor uw Huis</h2>
+                <h2 class="text-center header-font">Architecten voor uw Huis</h2>
 
-            <h3 class="text-center"> Ook wij bouwen ... aan onze nieuwe website</h3>
-        </div>
-    </div>
-    <header class="">
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation" style="z-index: 2; position: relative; ">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse"
-                        data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#section1">
-                    <img src="css/images/logoNav.png">
-                </a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#section1">home</a></li>
-                    <li><a href="#section2">profile</a></li>
-                    <li><a href="#section3">projecten</a></li>
-                    <li><a href="#section4">contact</a></li>
-                </ul>
+                <h3 class="text-center"> Ook wij bouwen ... aan onze nieuwe website</h3>
             </div>
         </div>
     </div>
-    </header>
+    <div id="header" class="header">
+        <header class="">
+            <div class="navbar navbar-default" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#section1">
+                            <img src="css/images/logoNav.png">
+                        </a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <div id="nav">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#section1">home</a></li>
+                                <li><a href="#section2">profile</a></li>
+                                <li><a href="#section3">projecten</a></li>
+                                <li><a href="#section4">contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
 
-        <div class="divider"></div>
-        <div class="row" id="section2">
+    <div class="divider"></div>
+    <div class="scrollto" id="section2">
+        <div class="row">
             <div class="col-sm-10 col-sm-offset-1">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <h1>Profiel</h1>
@@ -68,9 +71,13 @@
                         <h4> Architect / Zaakvoerder </h4><br>
 
                         <p> Studies interieurvormgeving aan St-Lukas te Brussel &nbsp; (2004-2007)</p>
+
                         <p> Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
+
                         <p>Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
+
                         <p>Nominatie: Young architect award &nbsp;(2011)</p>
+
                         <p>Zelfstandig architect sinds 2011 &nbsp;(2011)</p>
 
                     </div>
@@ -84,14 +91,14 @@
                     <div class="divider"></div>
                 </section>
             </div>
-
-
         </div>
-        <section class="bg-1">
-            <div class="col-sm-6 col-sm-offset-3 text-center"></div>
-        </section>
 
-        <div class="bg-4" id="section3">
+    </div>
+    <section class="bg-1">
+        <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+    </section>
+    <div class="scrollto" id="section3">
+        <div class="bg-4">
             <div class="container">
                 <div class="row">
                     <h1>Projecten</h1>
@@ -180,15 +187,17 @@
                 </div>
             </div>
         </div>
+    </div>
 
-        <section class="bg-2">
-            <div class="col-sm-6 col-sm-offset-3 text-center"></div>
-        </section>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-1">
-            </div>
+    <section class="bg-2">
+        <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+    </section>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-1">
         </div>
-        <div class="row" id="section4">
+    </div>
+    <div class="scrollto" id="section4">
+        <div class="row">
 
             <div class="col-sm-10 col-sm-offset-1">
                 <h1>Locatie</h1>
@@ -220,7 +229,7 @@
                 </address>
             </div>
         </div>
-
+    </div>
 
 
     <div class="modal" id="myModal" role="dialog">
@@ -261,5 +270,31 @@
 <script src="js/bootstrap.min.js"></script>
 <script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
 <script src="js/scripts.js"></script>
+<!-- all these references goes before the closing body tag-->
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="//cdn.jsdelivr.net/stickynavbar.js/1.2.0/jquery.stickyNavbar.min.js"></script>
+<script>
+    $(function () {
+        $('.header').stickyNavbar({
+            activeClass: "active",          // Class to be added to highlight nav elements
+            sectionSelector: "scrollto",    // Class of the section that is interconnected with nav links
+            animDuration: 250,              // Duration of jQuery animation
+            startAt: 0,                     // Stick the menu at XXXpx from the top of the this() (nav container)
+            easing: "linear",               // Easing type if jqueryEffects = true, use jQuery Easing plugin to extend easing types - gsgd.co.uk/sandbox/jquery/easing
+            animateCSS: true,               // AnimateCSS effect on/off
+            animateCSSRepeat: false,        // Repeat animation everytime user scrolls
+            cssAnimation: "fadeInDown",     // AnimateCSS class that will be added to selector
+            jqueryEffects: false,           // jQuery animation on/off
+            jqueryAnim: "slideDown",        // jQuery animation type: fadeIn, show or slideDown
+            selector: "a",                  // Selector to which activeClass will be added, either "a" or "li"
+            mobile: false,                  // If false nav will not stick under 480px width of window
+            mobileWidth: 480,               // The viewport width (without scrollbar) under which stickyNavbar will not be applied (due usability on mobile devices)
+            zindex: 9999,                   // The zindex value to apply to the element: default 9999, other option is "auto"
+            stickyModeClass: "sticky",      // Class that will be applied to 'this' in sticky mode
+            unstickyModeClass: "unsticky"   // Class that will be applied to 'this' in non-sticky mode
+        });
+    });
+</script>
 </body>
 </html>
