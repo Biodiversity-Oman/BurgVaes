336532f834b65e53cae207b921b1da219e7d9429
Tom Adriaens <tombeton@yahoo.com>
2015-05-20 10:10:37 +0200

    sticky header fix

--- online/BurgNew/index1.php
+++ online/BurgNew/index1.php
@@ -16,204 +16,206 @@
 </head>
 <body>
 <div id="wrapper">
-    <div class="first-container ">
+<header >
+    <div class="first-container " id="section1">
         <div class="v-center">
             <h1 class="text-center header-font"><b>Burg Vaes</b></h1>
+
             <h2 class="text-center header-font">Architecten voor uw Huis</h2>
 
             <h3 class="text-center"> Ook wij bouwen ... aan onze nieuwe website</h3>
         </div>
     </div>
-    <header class="">
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation" style="z-index: 2; position: relative; ">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse"
-                        data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#section1">
-                    <img src="css/images/logoNav.png">
-                </a>
+</header>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#section1">
+                <img src="css/images/logoNav.png">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-static-top  navbar-right">
+                <li><a href="#section1">home</a></li>
+                <li><a href="#section2">profile</a></li>
+                <li><a href="#section3">projecten</a></li>
+                <li><a href="#section4">contact</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="divider"></div>
+    <div class="row" id="section2">
+        <div class="col-sm-10 col-sm-offset-1">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <h1>Profiel</h1>
+                <hr>
+                <div class="col-sm-5">
+                    <h3>VAES Burghard</h3>
+                    <h4> Architect / Zaakvoerder </h4><br>
+
+                    <p> Studies interieurvormgeving aan St-Lukas te Brussel &nbsp; (2004-2007)</p>
+
+                    <p> Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
+
+                    <p>Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
+
+                    <p>Nominatie: Young architect award &nbsp;(2011)</p>
+
+                    <p>Zelfstandig architect sinds 2011 &nbsp;(2011)</p>
+
+                </div>
+                <div class="col-sm-2"></div>
+
+                <div class="col-sm-5">
+                    <img src="css/images/burg.jpg" class="img-responsive pull-right burg">
+                </div>
             </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#section1">home</a></li>
-                    <li><a href="#section2">profile</a></li>
-                    <li><a href="#section3">projecten</a></li>
-                    <li><a href="#section4">contact</a></li>
-                </ul>
+            <section>
+                <div class="divider"></div>
+            </section>
+        </div>
+
+
+    </div>
+    <section class="bg-1">
+        <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+    </section>
+
+    <div class="bg-4" id="section3">
+        <div class="container">
+            <div class="row">
+                <h1>Projecten</h1>
+                <hr>
+                <div class="col-sm-4 col-xs-6">
+
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><a href="#" title="Project 1"><img
+                                    src="css/images/1.jpg" class="img-responsive"></a></div>
+                        <div class="panel-body">
+                            <p></p>
+
+                            <p></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><a href="#" title="Project 2"><img
+                                    src="css/images/2.png" class="img-responsive"></a></div>
+                        <div class="panel-body">
+                            <p>project 1</p>
+
+                            <p></p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-xs-6">
+
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><a href="#" title="Project 3"><img
+                                    src="css/images/3.jpg" class="img-responsive"></a></div>
+                        <div class="panel-body">
+                            <p>project 3</p>
+
+                            <p></p>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-sm-4 col-xs-6">
+
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><a href="#" title="Project 4"><img
+                                    src="css/images/4.png" class="img-responsive"></a></div>
+                        <div class="panel-body">
+                            <p>project 4</p>
+
+                            <p></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><a href="#" title="Project 5"><img
+                                    src="css/images/5.jpg" class="img-responsive"></a></div>
+                        <div class="panel-body">
+                            <p>Project 5</p>
+
+                            <p></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><a href="#" title="Project 6"><img
+                                    src="css/images/6.jpg" class="img-responsive"></a></div>
+                        <div class="panel-body">
+                            <p>Project 6</p>
+
+                            <p>Project 6</p>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    </header>
 
-        <div class="divider"></div>
-        <div class="row" id="section2">
-            <div class="col-sm-10 col-sm-offset-1">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <h1>Profiel</h1>
-                    <hr>
-                    <div class="col-sm-5">
-                        <h3>VAES Burghard</h3>
-                        <h4> Architect / Zaakvoerder </h4><br>
-
-                        <p> Studies interieurvormgeving aan St-Lukas te Brussel &nbsp; (2004-2007)</p>
-                        <p> Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
-                        <p>Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
-                        <p>Nominatie: Young architect award &nbsp;(2011)</p>
-                        <p>Zelfstandig architect sinds 2011 &nbsp;(2011)</p>
-
-                    </div>
-                    <div class="col-sm-2"></div>
-
-                    <div class="col-sm-5">
-                        <img src="css/images/burg.jpg" class="img-responsive pull-right burg">
-                    </div>
-                </div>
-                <section>
-                    <div class="divider"></div>
-                </section>
-            </div>
-
-
+    <section class="bg-2">
+        <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+    </section>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-1">
         </div>
-        <section class="bg-1">
-            <div class="col-sm-6 col-sm-offset-3 text-center"></div>
-        </section>
+    </div>
+    <div class="row" id="section4">
 
-        <div class="bg-4" id="section3">
-            <div class="container">
-                <div class="row">
-                    <h1>Projecten</h1>
-                    <hr>
-                    <div class="col-sm-4 col-xs-6">
-
-                        <div class="panel panel-default">
-                            <div class="panel-thumbnail"><a href="#" title="Project 1"><img
-                                        src="css/images/1.jpg" class="img-responsive"></a></div>
-                            <div class="panel-body">
-                                <p></p>
-
-                                <p></p>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-xs-6">
-
-                        <div class="panel panel-default">
-                            <div class="panel-thumbnail"><a href="#" title="Project 2"><img
-                                        src="css/images/2.png" class="img-responsive"></a></div>
-                            <div class="panel-body">
-                                <p>project 1</p>
-
-                                <p></p>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-4 col-xs-6">
-
-                        <div class="panel panel-default">
-                            <div class="panel-thumbnail"><a href="#" title="Project 3"><img
-                                        src="css/images/3.jpg" class="img-responsive"></a></div>
-                            <div class="panel-body">
-                                <p>project 3</p>
-
-                                <p></p>
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="col-sm-4 col-xs-6">
-
-                        <div class="panel panel-default">
-                            <div class="panel-thumbnail"><a href="#" title="Project 4"><img
-                                        src="css/images/4.png" class="img-responsive"></a></div>
-                            <div class="panel-body">
-                                <p>project 4</p>
-
-                                <p></p>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-xs-6">
-
-                        <div class="panel panel-default">
-                            <div class="panel-thumbnail"><a href="#" title="Project 5"><img
-                                        src="css/images/5.jpg" class="img-responsive"></a></div>
-                            <div class="panel-body">
-                                <p>Project 5</p>
-
-                                <p></p>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-xs-6">
-
-                        <div class="panel panel-default">
-                            <div class="panel-thumbnail"><a href="#" title="Project 6"><img
-                                        src="css/images/6.jpg" class="img-responsive"></a></div>
-                            <div class="panel-body">
-                                <p>Project 6</p>
-
-                                <p>Project 6</p>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <section class="bg-2">
-            <div class="col-sm-6 col-sm-offset-3 text-center"></div>
-        </section>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-1">
-            </div>
-        </div>
-        <div class="row" id="section4">
-
-            <div class="col-sm-10 col-sm-offset-1">
-                <h1>Locatie</h1>
-                <hr>
-            </div>
-
-            <div id="map-canvas"></div>
-
+        <div class="col-sm-10 col-sm-offset-1">
+            <h1>Locatie</h1>
             <hr>
+        </div>
 
-            <div class="col-sm-8"></div>
-            <div class="col-sm-3 center-block">
+        <div id="map-canvas"></div>
 
-                <address>
-                    <h3>Burg Vaes Architecten</h3><br>
+        <hr>
+
+        <div class="col-sm-8"></div>
+        <div class="col-sm-3 center-block">
+
+            <address>
+                <h3>Burg Vaes Architecten</h3><br>
         <span id="map-input">
         Eduard Branlystraat 6<br>
         1190 Vorst</span><br>
-                </address>
+            </address>
 
-                <address>Steenweg 139<br>
-                    3540 Herk-de-Stad
-                </address>
-                0497/135700
-                <address>
-                    <br>
-                    <strong>stuur een mailtje</strong><br>
-                    <a href="mailto:#">info@burgvaes.be</a>
-                </address>
-            </div>
+            <address>Steenweg 139<br>
+                3540 Herk-de-Stad
+            </address>
+            0497/135700
+            <address>
+                <br>
+                <strong>stuur een mailtje</strong><br>
+                <a href="mailto:#">info@burgvaes.be</a>
+            </address>
         </div>
-
+    </div>
 
 
     <div class="modal" id="myModal" role="dialog">
@@ -258,6 +260,16 @@
     $("a").click(function () {
         return $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 300), !1
     });
+    $(document).scroll(function(e){
+        var scrollTop = $(document).scrollTop();
+        if(scrollTop > 0){
+            console.log(scrollTop);
+            $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+        } else {
+            $('.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+        }
+    });
+
 </script>
 </body>
 </html>
