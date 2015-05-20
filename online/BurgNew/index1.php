4052b11e9d10c0148386b3603f568cecd71fd7c6
Tom Adriaens <tombeton@yahoo.com>
2015-05-20 16:25:38 +0200

    gallery fixed

--- online/BurgNew/index1.php
+++ online/BurgNew/index1.php
@@ -11,22 +11,24 @@
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <link href="css/styles.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>
+    <!--    <link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>-->
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
 <div id="wrapper">
-<header >
-    <div class="first-container " id="section1">
-        <div class="v-center">
-            <h1 class="text-center header-font"><b>Burg Vaes</b></h1>
+    <header>
+        <div class="first-container " id="section1">
+            <div class="v-center">
+                <h1 class="text-center header-font">Burg Vaes</h1>
 
-            <h2 class="text-center header-font">Architecten voor uw Huis</h2>
+                <h3 class="text-center header-font-sub ">Architecten voor uw Huis</h3>
 
-            <h3 class="text-center"> Ook wij bouwen ... aan onze nieuwe website</h3>
+                <h4 class="text-center"> Ook wij bouwen ... aan onze nieuwe website</h4>
+            </div>
         </div>
-    </div>
-</header>
+    </header>
     <nav class="navbar navbar-default navbar-static-top" role="navigation" id="custom-nav">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse"
@@ -35,155 +37,158 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#section1">
-                <img src="css/images/logoNav.png">
-            </a>
+
         </div>
         <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-static-top  navbar-right">
+            <ul class="nav navbar-nav navbar-static-top  navbar-left nav-font">
                 <li><a href="#section1">home</a></li>
                 <li><a href="#section2">projecten</a></li>
                 <li><a href="#section3">profile</a></li>
                 <li><a href="#section4">contact</a></li>
+            </ul>
+            <ul class="pull-right">
+                <li><a class="navbar-brand" href="#section1">
+                        <img src="css/images/logoNav.png">
+                    </a></li>
             </ul>
         </div>
     </nav>
 
     <div class="divider"></div>
     <div class="bg-4" id="section2">
-        <div class="container">
-            <h1>Projecten</h1>
 
+        <section>
             <div class="row">
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <div class="caption">
-                            <h4>woning Bert en Kelly</h4>
-                            <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
-                        </div>
-                        <img src="css/images/BertenKelly.jpg" alt="...">
-                    </div>
-                </div>
+                <div class="col-sm-10 col-sm-offset-1">
+                    <h1>Projecten</h1>
 
-                <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Bert en Kelly</h4>
+
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
+                            </div>
+                            <img src="css/images/BertenKelly.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-3">
                         <div class="thumbnail">
                             <div class="caption">
                                 <h4>woning Burg en Ine</h4>
-                                <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
+
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
                             </div>
-                            <img src="css/images/Burgenine.jpg" alt="...">
+                            <img src="css/images/Burgenine.jpg" class="img-responsive" alt="...">
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="thumbnail">
-                                <div class="caption">
-                                    <h4>woning Danny</h4>
-                                    <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
-                                </div>
-                                <img src="css/images/Danny.jpg" alt="...">
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="thumbnail">
-                                    <div class="caption">
-                                        <h4>woning Dries en Sarina</h4>
-                                        <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
-                                    </div>
-                                    <img src="css/images/DriesenSarina.jpg" alt="...">
-                                </div>
-                            </div>
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Danny</h4>
 
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
+                            </div>
+                            <img src="css/images/Danny.jpg" class="img-responsive" alt="...">
                         </div>
                     </div>
+
+
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Dries en Sarina</h4>
+
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
+                            </div>
+                            <img src="css/images/DriesenSarina.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <div class="caption">
-                            <h4>woning Jerry en Uschi</h4>
-                            <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
-                        </div>
-                        <img src="css/images/JerryenUschi.JPG" alt="...">
-                    </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-md-3">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Jerry en Uschi</h4>
+
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
+                            </div>
+                            <img src="css/images/JerryenUschi.JPG" alt="...">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
                         <div class="thumbnail">
                             <div class="caption">
                                 <h4>woning Ronald en Simone</h4>
-                                <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
+
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
                             </div>
-                            <img src="css/images/RonaldenSimone.JPG" alt="...">
+                            <img src="css/images/RonaldenSimone.JPG" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Toon en Sara</h4>
+
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
+                            </div>
+                            <img src="css/images/ToonenSara.jpg" class="img-responsive" alt="...">
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="thumbnail">
-                                <div class="caption">
-                                    <h4>woning Toon en Sara</h4>
-                                    <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
-                                </div>
-                                <img src="css/images/ToonenSara.jpg" alt="...">
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="thumbnail">
-                                    <div class="caption">
-                                        <h4>vrije ontwerpen</h4>
-                                        <p><a href="" class="label label-danger" rel="tooltip" title="Zoom">naar het project</a>
-                                    </div>
-                                    <img src="css/images/Trailanderror.jpg" alt="...">
-                                </div>
-                            </div>
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>vrije ontwerpen</h4>
 
+                                <p><a href="" class="label label-danger" rel="tooltip">naar het project</a>
+                            </div>
+                            <img src="css/images/Trailanderror.jpg" class="img-responsive" alt="...">
                         </div>
                     </div>
                 </div>
+        </section>
+
+        <section class="bg-1">
+            <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+        </section>
+        <div class="row" id="section3">
+            <div class="col-sm-10 col-sm-offset-1">
+
+                <h1>Profiel</h1>
+
+                <div class="col-sm-5">
+                    <h3>VAES Burghard</h3>
+                    <h4> Architect / Zaakvoerder </h4><br>
+
+                    <p> Studies interieurvormgeving aan St-Lukas te Brussel &nbsp; (2004-2007)</p>
+
+                    <p> Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
+
+                    <p>Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
+
+                    <p>Nominatie: Young architect award &nbsp;(2011)</p>
+
+                    <p>Zelfstandig architect sinds 2011 &nbsp;(2011)</p>
+
+                </div>
+
+                <section>
+                    <div class="divider"></div>
+                </section>
             </div>
         </div>
-        </div>
     </div>
-
-    <section class="bg-1">
-        <div class="col-sm-6 col-sm-offset-3 text-center"></div>
-    </section>
-    <div class="row" id="section3">
-        <div class="col-sm-10 col-sm-offset-1">
-
-            <h1>Profiel</h1>
-            <hr>
-            <div class="col-sm-5">
-                <h3>VAES Burghard</h3>
-                <h4> Architect / Zaakvoerder </h4><br>
-
-                <p> Studies interieurvormgeving aan St-Lukas te Brussel &nbsp; (2004-2007)</p>
-
-                <p> Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
-
-                <p>Studies architectuur aan St-Lukas te Brussel &nbsp; (2007-2011)</p>
-
-                <p>Nominatie: Young architect award &nbsp;(2011)</p>
-
-                <p>Zelfstandig architect sinds 2011 &nbsp;(2011)</p>
-
-            </div>
-
-            <section>
-                <div class="divider"></div>
-            </section>
-        </div>
-    </div>
-
 
     <section class="bg-2">
         <div class="col-sm-6 col-sm-offset-3 text-center"></div>
@@ -196,12 +201,10 @@
 
         <div class="col-sm-10 col-sm-offset-1">
             <h1>Locatie</h1>
-            <hr>
         </div>
 
         <div id="map-canvas"></div>
 
-        <hr>
 
         <div class="col-sm-8"></div>
         <div class="col-sm-3 center-block">
@@ -225,34 +228,34 @@
         </div>
     </div>
 
-<!--
-    <div class="modal" id="myModal" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button class="close" type="button" data-dismiss="modal">×</button>
-                    <h3 class="modal-title"></h3>
-                </div>
-                <div class="modal-body">
-                    <div id="modalCarousel" class="carousel">
+    <!--
+        <div class="modal" id="myModal" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button class="close" type="button" data-dismiss="modal">×</button>
+                        <h3 class="modal-title"></h3>
+                    </div>
+                    <div class="modal-body">
+                        <div id="modalCarousel" class="carousel">
 
-                        <div class="carousel-inner">
+                            <div class="carousel-inner">
+
+                            </div>
+
+                            <a class="carousel-control left" href="#modaCarousel" data-slide="prev"><i
+                                    class="glyphicon glyphicon-chevron-left"></i></a>
+                            <a class="carousel-control right" href="#modalCarousel" data-slide="next"><i
+                                    class="glyphicon glyphicon-chevron-right"></i></a>
 
                         </div>
-
-                        <a class="carousel-control left" href="#modaCarousel" data-slide="prev"><i
-                                class="glyphicon glyphicon-chevron-left"></i></a>
-                        <a class="carousel-control right" href="#modalCarousel" data-slide="next"><i
-                                class="glyphicon glyphicon-chevron-right"></i></a>
-
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-default" data-dismiss="modal">Close</button>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
             </div>
-        </div>
-    </div>-->
+        </div>-->
     <footer id="footer">
         <center><a href="#section1"><img src="css/images/LogoBW.jpg"></a></center>
     </footer>
@@ -265,7 +268,7 @@
 <script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
 <script src="js/scripts.js"></script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         $(document).scroll(function (e) {
             var scrollTop = $(document).scrollTop();
             if (scrollTop > 0) {
@@ -281,10 +284,10 @@
         $("[rel='tooltip']").tooltip();
 
         $('.thumbnail').hover(
-            function(){
+            function () {
                 $(this).find('.caption').fadeIn(250); //.fadeIn(250)
             },
-            function(){
+            function () {
                 $(this).find('.caption').fadeOut(250); //.fadeOut(205)
             }
         );
