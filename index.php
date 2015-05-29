643476ad40101ea10b60a36bcc8b02827b803786
Tom Adriaens <tomadriaens@yahoo.com>
2015-05-29 13:40:28 +0200

    clean up 2

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>burg vaes architecten</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords"
+          content="burg, burghard, vaes, burg vaes, architect,alken, sint-lucas, sint-lukas, Architect Hasselt,
+          Architect Herk-de-Stad, Architect Limburg,Architect moderne woningen, Interieur architecten,Interieur architecten Hasselt,
+           Interieurarchitecten Limburg, Architectenbureau Hasselt, Architectenbureau Herk-de-Stad, Architectenbureau Limburg "/>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="css/styles.css" rel="stylesheet">
+
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <div class="first-container " id="section1">
+
+            <img src="css/images/homepageDraw.png" class="img-responsive absolute-center">
+
+        </div>
+    </header>
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" id="custom-nav">
+        <a class="navbar-brand pull-right" href="#section1">
+            <img src="css/images/logoNav.png" id="nav-logo">
+        </a>
+
+        <div class="navbar-header">
+
+            <button type="button" class="navbar-toggle pull-left" data-toggle="collapse"
+                    data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-static-top  navbar-left nav-font">
+                <li><a href="#section1">home</a></li>
+                <li><a href="#section2">projecten</a></li>
+                <li><a href="#section3">info</a></li>
+                <li><a href="#section4">locatie</a></li>
+                <li><a href="#section5">contact</a></li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <div class="divider"></div>
+
+
+    <section>
+        <div class="bg-4" id="section2">
+            <div class="row">
+                <h2 class="text-center">projecten</h2>
+
+                <div class="col-sm-10 col-sm-offset-1">
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Bert en Kelly</h4>
+
+                                <p><a href="bert&kelly.html" class="label btn-material-yellow" rel="tooltip">naar het
+                                        project</a>
+                            </div>
+                            <img src="css/images/BertenKelly.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Burg en Ine</h4>
+
+                                <p><a href="burg&ine.html" class="label  btn-material-yellow" rel="tooltip">naar het
+                                        project</a>
+                            </div>
+                            <img src="css/images/Burgenine.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Danny</h4>
+
+                                <p><a href="danny.html" class="label  btn-material-yellow" rel="tooltip">naar het
+                                        project</a>
+                            </div>
+                            <img src="css/images/Danny.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Dries en Sarina</h4>
+
+                                <p><a href="dries&sarina.html" class="label  btn-material-yellow" rel="tooltip">naar het
+                                        project</a>
+                            </div>
+                            <img src="css/images/DriesenSarina.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Jerry en Uschi</h4>
+
+                                <p><a href="jerry&uschi.html" class="label  btn-material-yellow" rel="tooltip">naar het
+                                        project</a>
+                            </div>
+                            <img src="css/images/JerryenUschi.JPG" alt="...">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Ronald en Simone</h4>
+
+                                <p><a href="ronald&simone.html" class="label  btn-material-yellow" rel="tooltip">naar
+                                        het project</a>
+                            </div>
+                            <img src="css/images/RonaldenSimone.JPG" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>woning Toon en Sara</h4>
+
+                                <p><a href="toon&sara.html" class="label  btn-material-yellow" rel="tooltip">naar het
+                                        project</a>
+                            </div>
+                            <img src="css/images/ToonenSara.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-3">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <h4>vrije ontwerpen</h4>
+
+                                <p><a href="vrije_ontwerpen.html" class="label  btn-material-yellow" rel="tooltip">naar
+                                        het project</a>
+                            </div>
+                            <img src="css/images/Trailanderror.jpg" class="img-responsive" alt="...">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="divider"></div>
+            <section class="bg-1">
+                <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+            </section>
+            <section id="section3">
+                <div class="row font-big">
+                    <div class="row">
+                        <div class="container">
+                            <h2 class="text-center">info </h2>
+
+                            <div class="col-lg-4  ">
+                                <ul>
+                                    <li><h5> architect / zaakvoerder </h5></li>
+                                    <li></li>
+                                    <li>master in de architectuur</li>
+
+                                    <li> campus Sint-Lucas Brussel, Faculteit architectuur.</li>
+
+                                    <li> afgestudeerd met onderscheiding 2009-2011</li>
+
+                                    <li>schakelprogramma van interieurvormgeving naar architectuur
+                                        Campus Sint-Lucas Brussel, Faculteit architectuur.
+                                        2007-2009
+                                    </li>
+
+                                    <li> bachelor interieurvormgeving</li>
+
+                                    <li> hogeschool Sint-Lukas Brussel 2004-2007</li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-4">
+
+
+                                <h5>architectenbureau Burg Vaes</h5>
+                                <h6>sinds januari 2012</h6>
+
+                                <ul>
+                                    <li>werk:</li>
+                                    <li> 3 nieuwbouwwoningen voor particiulieren // ontwerp, bouwaanvraag, uitvoering
+                                    </li>
+                                    <li> 9 renovaties voor particulieren // ontwerp, bouwaanvraag, uitvoering</li>
+                                    <li> 1 meergezinswoning voor particulier // ontwerp, bouwaanvraag</li>
+                                    <li> 1 kangoeroewoning voor particulier // ontwerp</li>
+                                </ul>
+
+                            </div>
+                            <div class="col-lg-4">
+
+                                <h5>stage als architect bij Axis Architecten & ingenieurs</h5>
+                                <h6>september 2012 tem december 2013</h6>
+                                <ul>
+                                    <li>werk:</li>
+                                    <li> 1 jeugdcentrum // ontwerp tot bouwaanvraag</li>
+                                    <li> 1 sociale woonwijk // werfopvolging</li>
+                                    <li> 1 magazijn- en kantoorgebouw // werfopvolging</li>
+                                    <li> 1 zorgcentrum // werfopvolging</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="row">
+                        <div class="container">
+                            <div class="col-lg-4">
+
+                                <h5> stage als architect bij studiebureau Rimanque</h5>
+                                <h6> november 2011 tot september 2012</h6>
+                                <ul>
+                                    <li>werk:</li>
+                                    <li>2 meergezinswoningen // bouwaanvraag</li>
+                                    <li> 1 school basisonderwijs // uitvoeringsplannen</li>
+                                    <li> 1 nieuwbouwwoning // ontwerp</li>
+                                    <li> 1 renovatie eengezinswoning // bouwaanvraag</li>
+                                    <li> 2 industrielen gebouwen // ontwerp, bouwaanvraag</li>
+                                    <li> 1 interieuropdracht // ontwerp, werf, uitgevoerd</li>
+                                </ul>
+
+                            </div>
+
+                            <div class="col-lg-4">
+
+
+                                <h5>stage als interieurvormgever bij Quinze and Milan</h5>
+                                <h6>juni-juli 2006</h6>
+                                <ul>
+                                    <li>werk:</li>
+                                    <li>1 interieuropdracht voor inrichting van een apotheek // ontwerp,
+                                        uitvoeringsplannen
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-4">
+
+                                <h5>handgemaakte tafel voor Design Brussels: Table Talks, onderleiding van Giovanna
+                                    Massoni</h5>
+                                <h6>juni-november 2007</h6>
+                                <ul>
+                                    <li>werk:</li>
+                                    <li> Ontwerp en eigenhandige uitvoering van een tafel tentoongesteld op de beurs
+                                        ‘design
+                                        Brussels’.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <section>
+                    <div class="divider"></div>
+                </section>
+
+
+                <section class="bg-2">
+                    <div class="col-sm-6 col-sm-offset-3 text-center"></div>
+                </section>
+                <div class="row">
+
+                    <div class="col-md-8 col-md-offset-1">
+                    </div>
+                </div>
+
+                <div class="row" id="section4">
+                    <section>
+                        <h2 class="text-center">locatie</h2>
+
+                        <div class="map-container">
+                            <img src="css/images/locatieA.png" class="img-responsive center-block">
+                        </div>
+
+
+                        <div class="row">
+                            <section id="section5">
+                                <h2 class="text-center">contact</h2>
+
+                                <div class="col-sm-4">
+                                </div>
+                                <div class="col-sm-4"></div>
+                                <div class="col-sm-4">
+                                    <ul>
+                                        <li><h3>architectenbureau burg vaes </h3><br></li>
+
+                                        <li><h5>steenweg 137</h5></li>
+                                        <li><h5>3540 herk-de-stad</h5></li>
+                                        <li>&nbsp;</li>
+                                        <li>0497 135 700</li>
+                                        <li>&nbsp;</li>
+                                        <li><h5>stuur een e-mail:</h5></li>
+                                        <li><a href="mailto:info@burgvaes.be"><h4 id="mail-mij">info@burgvaes.be</h4>
+                                            </a></li>
+                                        <li>
+                                            <a href="https://www.facebook.com/pages/Architectenbureau-Burg-Vaes/1674083962819378?ref=aymt_homepage_panel">
+                                                <img src="css/images/facebook-iconA.png"
+                                                     class="img-responsive text-center"></a>
+                                        </li>
+                                    </ul>
+                            </section>
+                        </div>
+                </div>
+
+                <footer id="footer">
+                    <center><a href="#section1"><img src="css/images/LogoBW.jpg"></a></center>
+                </footer>
+                <script src="js/jquery-2.1.4.min.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+                <script>
+                    $(document).ready(function () {
+                        $(document).scroll(function (e) {
+                            var scrollTop = $(document).scrollTop();
+                            if (scrollTop > 0) {
+                                $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+                            } else {
+                                $('.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+                            }
+                        });
+                        $("a").click(function () {
+                            return $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 300), !1
+                        });
+                        $("[rel='tooltip']").tooltip();
+
+                        $('.thumbnail').hover(
+                            function () {
+                                $(this).find('.caption').fadeIn(250); //.fadeIn(250)
+                            },
+                            function () {
+                                $(this).find('.caption').fadeOut(250); //.fadeOut(205)
+                            }
+                        );
+                    });
+
+                </script>
+</body>
+</html>
